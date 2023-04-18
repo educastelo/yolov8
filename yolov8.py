52eed19d85c378e367839554fbc761ead26a6d90
educastelo <dudu.sales08@hotmail.com>
2023-04-18 19:03:36 -0300

    update

--- yolov8.py
+++ yolov8.py
@@ -1,4 +1,5 @@
 import os
+import time
 import cv2
 import imutils
 import numpy as np
@@ -6,12 +7,12 @@ from ultralytics import YOLO
 from utilities.utils import point_in_polygons, draw_roi
 
 # setting the ROI (polygon) of the frame and loading the video stream
-points_polygon = [[[886, 489], [3827, 492], [3830, 2150], [12, 2153], [9, 1043], [889, 492]]]
-stream = 'traffic.webm'
+points_polygon = [[[317, 281], [294, 571], [1358, 535], [859, 283], [320, 281]],[[1625, 529], [2299, 791], [2342, 704], [1824, 494], [1623, 529]]]
+stream = u"rtmp://rtmp01.datavisiooh.com:1935/cartel_DN5502A"
 cap = cv2.VideoCapture(stream)
 
 # load the model and the COCO class labels our YOLO model was trained on
-model = YOLO("models/yolov8m.pt")
+model = YOLO("models/yolov8x.pt")
 labelsPath = os.path.sep.join(["coco", "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
@@ -22,13 +23,17 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 writer = None
 
 # set the confidence
-detection_threshold = 0.5
+detection_threshold = 0.1
 
 while True:
     ret, frame = cap.read()
 
+    start = time.time()
+
     # pass the frame to the yolov8 detector
-    results = model(frame)
+    results = model(source=frame, verbose=False)
+    end = time.time()
+    print("[INFO] classification time " + str((end - start) * 1000) + "ms")
 
     for result in results:
         detections = []
@@ -40,6 +45,9 @@ while True:
             x3 = int(x3)
             x4 = int(x4)
             class_id = int(class_id)
+
+            if LABELS[class_id] not in ["person", "car", "motorbike", "bicycle", "truck"]:
+                continue
 
             # Check if the centroid of each object is inside the polygon
             cX = (x1 + x3) / 2
@@ -55,25 +63,28 @@ while True:
 
             # draw a bounding box rectangle and label on the frame
             color = [int(c) for c in COLORS[int(class_id)]]
-            cv2.rectangle(frame, (x1, x2), (x3, x4), color, 2)
-            text = "{}: {:.4f}".format(LABELS[int(class_id)], score)
+            cv2.rectangle(frame, (x1, x2), (x3, x4), color, 6)
+            text = "{}: {:.2f}".format(LABELS[int(class_id)], score)
             cv2.putText(frame, text, (x1, x2 - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, color, 2)
-
-
-    ## save the video with detections
-    # if writer is None:
-    #     fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    #     writer = cv2.VideoWriter('traffic-yolov8.avi', fourcc, 30, (frame.shape[1], frame.shape[0]), True)
-    # writer.write(frame)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, color, 4)
 
     # draw roi
     output_frame = draw_roi(frame, points_polygon)
     resized = imutils.resize(output_frame, width=1200)
+    # resized = imutils.resize(frame, width=1200)
+
+    # # save the video with detections
+    # if writer is None:
+    #     fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    #     writer = cv2.VideoWriter('cartel_DN5002A.avi', fourcc, 15, (frame.shape[1], frame.shape[0]), True)
+    # writer.write(output_frame)
 
     # show the output
     cv2.imshow('Frame', resized)
     key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('k'):
+        cv2.imwrite("obelisco.jpg", frame)
 
     # stop the frame
     if key == ord('q'):
