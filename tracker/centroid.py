02e045be128d4b77698806e784ae45dee388d55a
educastelo <dudu.sales08@hotmail.com>
2023-10-24 22:31:13 -0300

    data.db

--- tracker/centroid.py
+++ tracker/centroid.py
@@ -23,7 +23,7 @@ import os
 
 # connect to database
 LOCAL_PATH = os.getcwd()
-NAME_DB = './count.db'
+NAME_DB = './data.db'
 PATH_DB = os.path.join(LOCAL_PATH, NAME_DB)
 sqliteConnection = sqlite3.connect(PATH_DB)
 cursor = sqliteConnection.cursor()
